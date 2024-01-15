1898aedc90aa19af0ce824403c346f95add95d1a
mrtnstl <spitellermarton@gmail.com>
2024-01-15 08:51:04 +0100

    commit

--- script.js
+++ script.js
@@ -89,18 +89,18 @@ function whatDayIsToday () {
     let date = new Date().toString();
     const dateSplit = date.split(" ");
     
-    console.log(dateSplit[0]);
+    /*console.log(dateSplit[0]);*/
     
     const translateDayName = {"Mon":"Hétfő", "Tue":"Kedd", "Wed":"Szerda", "Thu":"Csütörtök", "Fri":"Péntek", "Sat":"Szombat", "Sun":"Vasárnap"};
     
     //console.log(Object.keys(translateDayName))
-    console.log(translateDayName["Thu"]);
+    /*console.log(translateDayName["Thu"]);*/
     let today;
     for(let i = 0; i < Object.keys(translateDayName).length; i++){
         //console.log("loop")
-        console.log(translateDayName["Sun"])
+        /*console.log(translateDayName["Sun"])*/
         if(dateSplit[0] === Object.keys(translateDayName)[i]){
-            console.log("EGYEZÉS!!!" + translateDayName[Object.keys(translateDayName)[i]]);
+            /*console.log("EGYEZÉS!!!" + translateDayName[Object.keys(translateDayName)[i]]);*/
             today = translateDayName[Object.keys(translateDayName)[i]];
         } else{
             console.log("NINCS EGYEZÉS")
@@ -113,7 +113,7 @@ function whatDayIsToday () {
             //dayToHighlight[i].style.background = "linear-gradient(20deg, red, blue)";
             dayToHighlight[i].style.textDecoration = "underline";
         } else {}
-        console.log(dayToHighlight[i].innerHTML)
+        /*console.log(dayToHighlight[i].innerHTML)*/
     }
 }
 whatDayIsToday();
